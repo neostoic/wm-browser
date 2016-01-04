5fb55859f39cfd5979b2b40c5368eb79567805b7
Amrit Panesar <apanesar@4195tech.com>
2016-01-03 23:11:50 -0800

    App\Weedmaps

--- app/Http/Controllers/APISearchController.php
+++ app/Http/Controllers/APISearchController.php
@@ -64,7 +64,7 @@ class APISearchController extends BaseController
     		],
     	];
 
-    	$results = Weedmaps::searchByQuery($query);
+    	$results = \App\Weedmaps::searchByQuery($query);
 
     	dd($results);
     }
